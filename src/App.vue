ee906dc5d6cb60138f87969cfe2fdcb3ae5c794d
何喜 <877073978@qq.com>
2021-03-29 16:32:36 +0800

    项目准备

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  created () {
+    axios({
+      url: process.env.VUE_APP_BASE_API + '/sys/login',
+      method: 'post',
+      data () {
+        return {
+          mobile: '1360000002',
+          password: '123456'
+        }
+      }
+    }).then(res => {
+      //成功回调
+      console.log(res)
+    })
+  }
+}
+</script>
